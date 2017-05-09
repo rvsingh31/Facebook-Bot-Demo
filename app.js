4676ec13ccc61ec6691473147d04dae5c469888c
rvsingh31 <rvsingh_31@yahoo.com>
2017-05-10 01:11:42 +0530

    App.js updates

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get("/", function (req, res) {
 
 // Facebook Webhook
 // Used for verification
-app.get("/webhook", function (req, res) {
+app.post("/webhook", function (req, res) {
  /* if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
     console.log("Verified webhook");
     res.status(200).send(req.query["hub.challenge"]);
