3105731efb7e66ed1d6c52473fc5e82eedc08798
rvsingh31 <rvsingh_31@yahoo.com>
2017-05-10 00:24:04 +0530

    App.js updates

--- app.js
+++ app.js
@@ -15,11 +15,72 @@ app.get("/", function (req, res) {
 // Facebook Webhook
 // Used for verification
 app.get("/webhook", function (req, res) {
-  if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
+ /* if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
     console.log("Verified webhook");
     res.status(200).send(req.query["hub.challenge"]);
   } else {
     console.error("Verification failed. The tokens do not match.");
     res.sendStatus(403);
   }
+  
+  */
+  
+  if(req.body.object=="page")
+  {
+	  req.body.entry.forEach(function(entry){
+		  entry.messaging.forEach(function(event){
+			  if(event.postback)
+			  {
+				  processPostback(event);
+			  }
+		  })
+	  })
+	  res.sendStatus(200);
+  }
 });
+
+function processPostback(event){
+	var senderId=event.sender.id;
+	var payload=event.postback.payload;
+	if(payload=="Greeting")
+	{
+		request({
+			url:"https://graph.facebook.com/v2.6/"+senderId,
+			qs:{
+				access_token:process.env.PAGE_ACCESS_TOKEN,
+				fields:"first_name"
+			},
+			method:"GET"
+		},function(error,response,body){
+			var greeting="";
+			if(error)
+			{
+				console.log("Error occured:"+error);
+			}
+			else
+			{
+				var bodyObj=JSON.parse(body);
+				name=bodyObj.first_name;
+				greeting="HI "+name+". ";
+			}
+			var message=greeting+" My name is SP MOVIES.";
+			sendMessage(senderId,{text:message});
+		});
+	}
+	
+}
+function sendMessage(recipientId, message) {
+  request({
+    url: "https://graph.facebook.com/v2.6/me/messages",
+    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+    method: "POST",
+    json: {
+      recipient: {id: recipientId},
+      message: message,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log("Error sending message: " + response.error);
+    }
+  });
+}
