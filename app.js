e368ffb3fb1476454e8d9bebff8dabdaad1f036e
rvsingh31 <rvsingh_31@yahoo.com>
2017-05-10 01:00:54 +0530

    App.js updates

--- app.js
+++ app.js
@@ -15,62 +15,57 @@ app.get("/", function (req, res) {
 // Facebook Webhook
 // Used for verification
 app.get("/webhook", function (req, res) {
-  if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
+ /* if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
     console.log("Verified webhook");
     res.status(200).send(req.query["hub.challenge"]);
   } else {
     console.error("Verification failed. The tokens do not match.");
     res.sendStatus(403);
   }
-  
- /* 
-  
-  if(req.body.object=="page")
-  {
-	  req.body.entry.forEach(function(entry){
-		  entry.messaging.forEach(function(event){
-			  if(event.postback)
-			  {
-				  processPostback(event);
-			  }
-		  })
-	  })
-	  res.sendStatus(200);
-  }
- */ 
-  
+  */
+  if (req.body.object == "page") {
+    req.body.entry.forEach(function(entry) {
+      entry.messaging.forEach(function(event) {
+        if (event.postback) {
+          processPostback(event);
+        }
+      });
+    });
+
+    res.sendStatus(200);
+  } 
 });
 
-function processPostback(event){
-	var senderId=event.sender.id;
-	var payload=event.postback.payload;
-	if(payload=="Greeting")
-	{
-		request({
-			url:"https://graph.facebook.com/v2.6/"+senderId,
-			qs:{
-				access_token:process.env.PAGE_ACCESS_TOKEN,
-				fields:"first_name"
-			},
-			method:"GET"
-		},function(error,response,body){
-			var greeting="";
-			if(error)
-			{
-				console.log("Error occured:"+error);
-			}
-			else
-			{
-				var bodyObj=JSON.parse(body);
-				name=bodyObj.first_name;
-				greeting="HI "+name+". ";
-			}
-			var message=greeting+" My name is SP MOVIES.";
-			sendMessage(senderId,{text:message});
-		});
-	}
-	
+function processPostback(event) {
+  var senderId = event.sender.id;
+  var payload = event.postback.payload;
+
+  if (payload === "Greeting") {
+    // Get user's first name from the User Profile API
+    // and include it in the greeting
+    request({
+      url: "https://graph.facebook.com/v2.6/" + senderId,
+      qs: {
+        access_token: process.env.PAGE_ACCESS_TOKEN,
+        fields: "first_name"
+      },
+      method: "GET"
+    }, function(error, response, body) {
+      var greeting = "";
+      if (error) {
+        console.log("Error getting user's name: " +  error);
+      } else {
+        var bodyObj = JSON.parse(body);
+        name = bodyObj.first_name;
+        greeting = "Hi " + name + ". ";
+      }
+      var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+      sendMessage(senderId, {text: message});
+    });
+  }
 }
+
+// sends message to user
 function sendMessage(recipientId, message) {
   request({
     url: "https://graph.facebook.com/v2.6/me/messages",
