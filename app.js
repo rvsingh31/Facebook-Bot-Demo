8ef1f6b927e8ad6920551a41be868fd0a1837761
rvsingh31 <rvsingh_31@yahoo.com>
2017-05-11 19:57:14 +0530

    app.js updated

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function processPostback(event) {
         name = bodyObj.first_name;
         greeting = "Hi " + name + ". ";
       }
-      var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+      var message = greeting + "My name is Football Notifications Bot. I can tell you various updates regarding International Teams and various Clubs news . To subscribe to our newsletter :\n Type in : #subscribe \n\n Thank You.";
       sendMessage(senderId, {text: message});
     });
   }
@@ -91,41 +91,48 @@ function processM(event) {
 
 		if (message.text) {
 			var formattedMsg = message.text.toLowerCase().trim();
-	
-	
-			request({
-				url: "https://graph.facebook.com/v2.6/" + senderId,
-				qs: {
-					access_token: process.env.PAGE_ACCESS_TOKEN,
-					fields: "first_name"
-				},
-				method: "GET"
-			}, function(error, response, body) {
-				var g="";
-				var name="";
-				if (error) {
-					console.log("Error getting user's name: " +  error);
-				} else {
-					var bodyObj = JSON.parse(body);
-					name = bodyObj.first_name;
-					g = "Hi " + name + ". \n";
-				}
-					var msg=g+"You have been successfully subscribed and we'll provide you latest updates time to time.\n Thank You for choosing Football Notifications.";
-					var user={user_id:senderId,first_name:name};
-					var query=movie(user);
-					query.save(function(err){
-						if (err) {console.error(err);
-							if(err.name="ValidatorError")
-							{
-								sendMessage(senderId, {text: "You are already subscribed."});
+			if(formattedMsg=="#subscribe")
+			{
+					
+				request({
+					url: "https://graph.facebook.com/v2.6/" + senderId,
+					qs: {
+						access_token: process.env.PAGE_ACCESS_TOKEN,
+						fields: "first_name"
+					},
+					method: "GET"
+				}, function(error, response, body) {
+					var g="";
+					var name="";
+					if (error) {
+						console.log("Error getting user's name: " +  error);
+					} else {
+						var bodyObj = JSON.parse(body);
+						name = bodyObj.first_name;
+						g = "Hi " + name + ". \n";
+					}
+						var msg=g+"You have been successfully subscribed and we'll provide you latest updates time to time.\n Thank You for choosing Football Notifications.";
+						var user={user_id:senderId,first_name:name};
+						var query=movie(user);
+						query.save(function(err){
+							if (err) {console.error(err);
+								if(err.name="ValidatorError")
+								{
+									sendMessage(senderId, {text: "You are already subscribed."});
+								}
 							}
-						}
-						else
-						{
-							sendMessage(senderId, {text: msg});
-						}
-					});
-			});
+							else
+							{
+								sendMessage(senderId, {text: msg});
+							}
+						});
+				});
+			}
+			else
+			{
+				sendMessage(senderId, {text: "Sorry, We couldn't complete your request. Try Again."});
+			}
+
 		} else if (message.attachments) {
 			sendMessage(senderId, {text: "Sorry, I don't understand your request."});
 		}
