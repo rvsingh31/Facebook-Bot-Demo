dff72de30172eb0027759766019d4b3541a55057
rvsingh31 <rvsingh_31@yahoo.com>
2017-05-11 19:48:45 +0530

    duplicates avoided

--- movie.js
+++ movie.js
@@ -1,9 +1,10 @@
 var mongoose=require("mongoose");
+var uniqueValidator = require('mongoose-unique-validator');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-  user_id: {type: String},
-  first_name:{type:String}
+  user_id: {type: String,unique:true},
+  first_name:{type:String,unique:true}
 });
-
+UserSchema.plugin(uniqueValidator);
 module.exports = mongoose.model("users", UserSchema);
